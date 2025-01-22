1774dc36dda9c98816896e88d0b855652bdbd8ff
sujikim1130 <sujikim2013@gmail.com>
2025-01-23 01:44:41 +0900

    프래그먼트 예시

--- app/src/main/java/com/example/guru_8/fragment/SettingsFragment.kt
+++ app/src/main/java/com/example/guru_8/fragment/SettingsFragment.kt
@@ -1,17 +1,18 @@
-package com.example.guru_8
+package com.example.guru_8.fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.guru_8.R
 
 
-class HomeFragment : Fragment() {
+class SettingsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_settings, container, false)
     }
 }
