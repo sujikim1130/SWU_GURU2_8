94550101d20116ee92133b724c92795d454ded6c
sujikim1130 <sujikim2013@gmail.com>
2025-01-23 01:44:41 +0900

    프래그먼트 예시

--- app/src/main/java/com/example/guru_8/fragment/StatsFragment.kt
+++ app/src/main/java/com/example/guru_8/fragment/StatsFragment.kt
@@ -1,10 +1,11 @@
-package com.example.guru_8
+package com.example.guru_8.fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.guru_8.R
 
 
 class StatsFragment : Fragment() {
