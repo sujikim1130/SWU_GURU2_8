4695e7288c794d3412e4f4918587f8c7d7a185fd
Hyjii <124658283+Hyjii@users.noreply.github.com>
2025-01-23 12:34:53 +0900

    sub3 통계 기능, 통계 환경설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,13 +4,14 @@ plugins {
 }
 
 android {
-    namespace = "com.example.guru2"
+    namespace = "com.example.guru_8"
     compileSdk = 35
 
+
     defaultConfig {
-        applicationId = "com.example.guru2"
+        applicationId = "com.example.guru_8"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -33,10 +34,19 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+//xml과 앱의 데이터 소스를 직접 연결하기 위해 추가
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
-dependencies {
 
+dependencies {
+    val fragment_version = "1.8.3"
+    // Java language implementation
+    implementation("androidx.fragment:fragment:$fragment_version")
+    // Kotlin
+    implementation("androidx.fragment:fragment-ktx:$fragment_version")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -48,3 +58,9 @@ dependencies {
 
     implementation(libs.mpandroidchart) //추가
 }
+
+//dependencies {
+//    implementation 'androidx.appcompat:appcompat:1.6.1
+//    implementation 'androidx.fragment:fragment-ktx:1.5.7'
+//    implementation 'com.google.android.material:material:1.9.0'
+//}
